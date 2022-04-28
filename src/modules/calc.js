c317d598fbe8593fd3a5df143233f0fe5903bbab
Təvəkkül <tevizeynalov@gmail.com>
2022-04-28 14:55:25 +0400

    сделал ОБЯЗАТЕЛЬНОЕ и усложненное

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,3 +1,7 @@
+import {
+    animate
+} from './helpers';
+
 const calc = (price = 100) => {
     const calcBlock = document.querySelector('.calc-block');
     const calcType = document.querySelector('.calc-type');
@@ -9,6 +13,7 @@ const calc = (price = 100) => {
     const countCalc = () => {
         const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
         const calcSquareValue = calcSquare.value;
+
         let totalValue = 0;
         let calcCountValue = 1;
         let calcDayValue = 1;
@@ -16,37 +21,37 @@ const calc = (price = 100) => {
         if (calcCount.value > 1) {
             calcCountValue += +calcCount.value / 10;
         }
+
         if (calcDay.value && calcDay.value < 5) {
             calcDayValue = 2;
         } else if (calcDay.value && calcDay.value < 10) {
             calcDayValue = 1.5;
         }
+
         if (calcType.value && calcSquare.value) {
             totalValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
         } else {
             totalValue = 0;
         }
-        total.textContent = totalValue;
+
+        return totalValue;
     };
 
-    function animateValue(obj, start, end, duration) {
-        let startTimestamp = null;
-        const step = (timestamp) => {
-            if (!startTimestamp) startTimestamp = timestamp;
-            const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-            obj.innerHTML = Math.floor(progress * (end - start) + start);
-            if (progress < 1) {
-                window.requestAnimationFrame(step);
-            }
-        };
-        window.requestAnimationFrame(step);
-    }
-    calcBlock.addEventListener('input', (e) => {
+    calcBlock.addEventListener('input', e => {
         if (e.target === calcType || e.target === calcSquare ||
             e.target === calcCount || e.target === calcDay) {
-            countCalc();
-            animateValue(total, 0, total.textContent, 500);
+            const totalValue = countCalc();
+            animate({
+                duration: 500,
+                timing(timeFraction) {
+                    return timeFraction;
+                },
+                draw(progress) {
+                    total.textContent = Math.round(progress * totalValue);
+                }
+            });
         }
     });
 };
+
 export default calc;
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,52 +1,51 @@
+import {
+	animate
+} from './helpers';
+
 const modal = () => {
 	const buttons = document.querySelectorAll('.popup-btn');
 	const modal = document.querySelector('.popup');
 	const modalContent = modal.querySelector('.popup-content');
 
-	let count = 0;
-	let idInterval;
-	let isOpen = false;
-
-	const modalAnimation = () => {
-		if (window.innerWidth > 768) {
-			if (isOpen) {
-				count++;
-				modal.style.display = 'block';
-				idInterval = requestAnimationFrame(modalAnimation);
-				if (count <= 100) {
-					modalContent.style.opacity = count * 4 + '%';
-				} else {
-					cancelAnimationFrame(idInterval);
-					count = 100;
-				}
-				modalContent.style.transform = 'translateX(100px)';
-			} else {
-				count -= 4;
-				idInterval = requestAnimationFrame(modalAnimation);
-				if (count >= 0) {
-					modalContent.style.opacity = count / 2 + '%';
-				} else {
-					cancelAnimationFrame(idInterval);
-					modal.style.display = 'none';
-					count = 0;
-				}
-			}
-		} else {
-			modal.style.display = modal.style.display === 'none' ? 'block' : 'none';
-		}
-	};
-
 	buttons.forEach(button => {
 		button.addEventListener('click', () => {
-			isOpen = !isOpen;
-			modalAnimation();
+			if (window.innerWidth > 768) {
+				modal.style.display = 'block';
+				modalContent.style.transform = 'translateX(100px)';
+
+				animate({
+					duration: 500,
+					timing(timeFraction) {
+						return timeFraction;
+					},
+					draw(progress) {
+						modalContent.style.opacity = progress;
+					}
+				});
+			} else {
+				modal.style.display = 'block';
+			}
 		});
 	});
 
 	modal.addEventListener('click', e => {
 		if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
-			isOpen = !isOpen;
-			modalAnimation();
+			if (window.innerWidth > 768) {
+				animate({
+					duration: 300,
+					timing(timeFraction) {
+						return timeFraction;
+					},
+					draw(progress) {
+						modalContent.style.opacity = 1 - progress;
+					}
+				});
+			} else {
+				modal.style.display = 'none';
+			}
+			setTimeout(() => {
+				modal.style.display = 'none';
+			}, 300);
 		}
 	});
 };
